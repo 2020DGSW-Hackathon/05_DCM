e4a6d40bd9b64fe5ff049b1d0912c339a99f834f
KangMinseok <qbq5000@gmail.com>
2020-09-08 14:33:08 +0900

    Initial Commit

---
+++ DCM_iOS/DCM_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DCM_iOS
+//
+//  Created by 강민석 on 2020/09/08.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
