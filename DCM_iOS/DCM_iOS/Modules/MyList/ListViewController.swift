a63338898169d814176605894085fbe19769abe7
KangMinseok <qbq5000@gmail.com>
2020-11-06 23:53:09 +0900

    [FIX] 테이블뷰 deselect관련 설정

--- DCM_iOS/DCM_iOS/Modules/MyList/ListViewController.swift
+++ DCM_iOS/DCM_iOS/Modules/MyList/ListViewController.swift
@@ -116,4 +116,8 @@ extension ListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
